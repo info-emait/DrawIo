71fda2c7e65f43c3c2472eb294c2d15c92a4e7c9
Šutka Martin <info.emait@gmail.com>
2026-05-12 15:05:18 +0200

    Added script for manifest generation

---
+++ scripts/generateExtensionManifest.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+import path from "path";
+
+const root = process.cwd();
+const packageJson = JSON.parse(fs.readFileSync(path.join(root, "package.json"), "utf-8"));
+let manifest = fs.readFileSync(path.join(root, "manifests", "vss-extension.json"), "utf-8");
+
+manifest = manifest
+    .replaceAll("#{Id}#", packageJson.name)
+    .replaceAll("#{Version}#", packageJson.version)
+    .replaceAll("#{Publisher}#", packageJson.author)
+    .replaceAll("#{Description}#", packageJson.description);
+
+fs.writeFileSync(
+    path.join(root, "dist", "vss-extension.json"),
+    manifest
+);
+
+console.log("vss-extension.json generated");
