9f7faf0896efd3d61807533d5e39e31d1bef4c07
Šutka Martin <info.emait@gmail.com>
2026-05-14 18:49:45 +0200

    Refactor of utils

---
+++ src/utils/devops.js
@@ -0,0 +1,97 @@
+import * as sdk from "azure-devops-extension-sdk";
+import { CommonServiceIds } from "azure-devops-extension-api";
+
+//#region [ Fields ]
+
+let _contextCache = null;
+let _contextPromise = null;
+
+//#endregion
+
+
+//#region [ Methods : Private ]
+
+/**
+ * Gets the base url.
+ * 
+ * @returns Base url.
+ */
+function _getBaseUrl() {
+    try {
+        if (document.referrer) {
+            const refUrl = new URL(document.referrer);
+            return refUrl.protocol + "//" + refUrl.host;
+        }
+    } 
+    catch (e) {
+    }
+
+    try {
+        const url = new URL(window.location.href);
+        return url.protocol + "//" + url.host;
+    }
+    catch (e) {
+    }
+
+    return "";
+}
+
+//#endregion
+
+
+/**
+ * Gets DevOps context.
+ * 
+ * @returns Object representing DevOps context.
+ */
+export async function context() {
+    if (_contextCache) {
+        return _contextCache;
+    }
+
+    if (_contextPromise) {
+        return _contextPromise;
+    }
+
+    _contextPromise = (async () => {
+        const baseUrl = _getBaseUrl();
+
+        const page = await sdk.getService(CommonServiceIds.ProjectPageService);
+        const project = await page.getProject();
+        const collection = sdk.getHost? sdk.getHost() : null;
+        const extension = sdk.getExtensionContext();
+    
+        _contextCache = {
+            baseUrl: baseUrl,
+            isCloud: (baseUrl.indexOf("dev.azure.com") !== -1) || (baseUrl.indexOf("visualstudio.com") !== -1),
+            project,
+            collection,
+            extension
+        };
+
+        return _contextCache;
+    })();
+
+    return _contextPromise;
+}
+
+
+/**
+ * Makes GET xhr request.
+ * 
+ * @param {string} uri Azure DevOps REST Api url segement.
+ * @param {object} query Query string parameters.
+ * @returns Promise.
+ */
+export async function get(uri, query) {
+    const ctx = await context();
+    const token = await sdk.getAccessToken();
+    const prms = {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    };
+    
+    return fetch(`${ctx.baseUrl}/${ctx.collection.name}/${ctx.project.name}${uri}?${new URLSearchParams(query || {})}`, prms);
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
 export * from "./log";
 export * from "./ready";
 export * from "./registerComponent";
-export * from "./getDevOpsContext";
+export * from "./devops";
